7307cb40faca3acb45db26dbc7fd1cfbafa5e1b3
Ivan Husarov <ivan.husarov@gmail.com>
2021-07-26 09:08:35 +0300

    migrations update

--- prisma/migrations/20210726054344_init/migration.sql
+++ prisma/migrations/20210726054344_init/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE "user" (
     "email" TEXT NOT NULL,
     "name" TEXT,
     "image" TEXT,
+    "online" BOOLEAN NOT NULL DEFAULT false,
 
     PRIMARY KEY ("id")
 );
@@ -19,6 +20,19 @@ CREATE TABLE "userposition" (
     PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "filerequest" (
+    "id" TEXT NOT NULL,
+    "senderid" TEXT NOT NULL,
+    "receiverid" TEXT NOT NULL,
+    "updatedat" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "size" INTEGER NOT NULL,
+    "accepted" BOOLEAN,
+
+    PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "user.email_unique" ON "user"("email");
 
@@ -27,3 +41,9 @@ CREATE UNIQUE INDEX "userposition.userid_unique" ON "userposition"("userid");
 
 -- AddForeignKey
 ALTER TABLE "userposition" ADD FOREIGN KEY ("userid") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "filerequest" ADD FOREIGN KEY ("senderid") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "filerequest" ADD FOREIGN KEY ("receiverid") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
